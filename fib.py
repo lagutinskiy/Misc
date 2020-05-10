115f1613a462d1e097d81ff8acc01c6ee93187cc
Vladimir Lagutinskiy <vladimirlagutinskiy@Vladimirs-MacBook-Pro.local>
2020-05-10 17:13:01 +0200

    fibonachi function

---
+++ fib.py
@@ -0,0 +1,11 @@
+def fib(n):
+    total = 0
+    if n < 0:
+        total = 0
+    elif n == 1:
+        total = 1
+    else:
+        total = fib(n-2) + fib(n-1)
+    return total
+
+print(fib(25))
